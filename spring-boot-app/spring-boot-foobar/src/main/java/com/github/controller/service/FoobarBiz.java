6d6c2017f64ead01e566e0d23ee328d982b869d5
zhanghang32 <zhanghang32@meituan.com>
2022-03-16 22:36:03 +0800

    test log level

--- spring-boot-app/spring-boot-foobar/src/main/java/com/github/controller/service/FoobarBiz.java
+++ spring-boot-app/spring-boot-foobar/src/main/java/com/github/controller/service/FoobarBiz.java
@@ -3,8 +3,13 @@ package com.github.controller.service;
 import com.github.controller.exceptions.GlobalException;
 import com.github.controller.service.common.BizTemplate;
 import com.github.controller.service.common.monitor.Monitors;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * @author hangs.zhang
@@ -13,12 +18,23 @@ import org.springframework.stereotype.Service;
  * function:
  */
 @Service
+@SuppressWarnings("all")
 public class FoobarBiz {
 
+    protected static Logger logger = LoggerFactory.getLogger(FoobarBiz.class);
+
     public String bizMethod(final String data) {
+
+        logger.info("bizMethod info log");
+        logger.debug("bizMethod debug log");
+
         return new BizTemplate<String>(Monitors.inner_error) {
             @Override
+            @SneakyThrows
             protected String process() throws GlobalException {
+                logger.info("bizMethod info log");
+                logger.debug("bizMethod debug log");
+                Thread.sleep(new Random().nextInt(100));
                 return "deal:" + data;
             }
 
